fa50a44c5733f088dc237fd4736124ac98f9e1ec
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-07-30 16:31:24 +0530

    Home Page Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from '/src/pages/Home';
 import Movies from '/src/pages/Movies';
 import MovieDetails from '/src/pages/MovieDetails';
@@ -7,12 +7,19 @@ import SeatLayout from '/src/pages/SeatLayout';
 import MyBookings from '/src/pages/MyBookings';
 import Favorite from '/src/pages/Favorite';
 import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
+
 
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  
   return (
     <>
-    <Navbar />
+    <Toaster/>
+    {!isAdminRoute && <Navbar />}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/movies' element={<Movies/>} />
@@ -21,6 +28,7 @@ const App = () => {
         <Route path='/my-bookings' element={<MyBookings/>} />
         <Route path='/favorite' element={<Favorite/>} />
       </Routes>
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection';
 import FeaturedSection from '../components/FeaturedSection';
+import TrailerSection from '../components/TrailerSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <FeaturedSection />
+      <TrailerSection />
     </div>
   )
 }
