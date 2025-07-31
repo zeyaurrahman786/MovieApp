e708642d1a0b30176d00ceff9be5805bbcbdbd82
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-07-31 16:20:46 +0530

    Movies and Favorite

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { dummyShowsData } from '../assets/assets';
+import MovieCard from '../components/MovieCard';
+import BlurCircle from '../components/BlurCircle';
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
+  return dummyShowsData.length > 0 ?  (
+    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h[80vh]'>
+
+      <BlurCircle top='150px' left='0px' />
+      <BlurCircle bottom='50px' right='50px' />
+
+        <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+        <div className='flex flex-wrap max-sm:justify-center gap-8'>
+          {dummyShowsData.map((movie) => (
+            <MovieCard movie={movie} key={movie._id}/>
+          ))}
+        </div>
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-3xl font-bold text-center'>No movies available</h1>
+    </div>
   )
 }
 
