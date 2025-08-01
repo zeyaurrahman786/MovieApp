f8092cf4d95fb90ea423b305a618eef0d3bf44cc
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-08-01 16:17:09 +0530

    MovieDetails

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
+import Loading from '../components/Loading';
+import { Heart, PlayCircle, StarIcon } from "lucide-react";
+import timeFormat from '../library/timeFormat';
+import BlurCircle from '../components/BlurCircle';
 
 const MovieDetails = () => {
-  return (
-    <div>MovieDetails</div>
-  )
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    if(show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData,
+      })
+    }
+  };
+
+  useEffect(() => {
+    getShow();
+  }, [id]);
+
+  return show ? (
+    <div className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
+      <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
+
+        <img src={show.movie.poster_path} alt="" className="max-md:mx-auto rounded-xl h-104 max-w-70 object-cover" />
+
+        <div className="relative flex flex-col gap-3">
+          <BlurCircle top="-100px" left="-100px" />
+          <p className="text-primary">ENGLISH</p>
+          <h1 className="text-4xl font-semibold max-w-96 text-balance">{show.movie.title}</h1>
+
+          <div className="flex items-center gap-2 text-gray-300">
+            <StarIcon className="w-5 h-5 text-primary fill-primary" />
+            {show.movie.vote_average.toFixed(1)} User Rating
+          </div>
+
+          <p className="text-gray-400 mt-2 text-sm leading-tight max-w-xl">{show.movie.overview}</p>
+
+          <p>
+            {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre => 
+              genre.name).join(", ")} • {show.movie.release_date.split("_")[0]}
+          </p>
+
+          <div className="flex items-center flex-wrap gap-4 mt-4">
+            <button className="flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95">
+              <PlayCircle className="w-5 h-5" />
+              Watch Trailer
+            </button>
+            <a href="#dateSelect" className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95">Buy Tickets</a>
+            <button className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
+              <Heart className={`w-5 h-5`} />
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  ) : <Loading />
 }
 
 export default MovieDetails;
