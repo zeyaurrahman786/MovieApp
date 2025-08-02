b064eca03655212eba5d4358a2f23c3330d5c7ae
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-08-02 15:34:30 +0530

    MovieDetails

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -5,6 +5,8 @@ import Loading from '../components/Loading';
 import { Heart, PlayCircle, StarIcon } from "lucide-react";
 import timeFormat from '../library/timeFormat';
 import BlurCircle from '../components/BlurCircle';
+import MovieCard from '../components/MovieCard';
+
 
 const MovieDetails = () => {
 
@@ -59,9 +61,32 @@ const MovieDetails = () => {
               <Heart className={`w-5 h-5`} />
             </button>
           </div>
-
         </div>
       </div>
+
+      <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0,12).map((cast, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img src={cast.profile_path} alt="" className="rounded-full h-20 md:h-20 aspect-square object-cover" />
+              <p className="font-medium text-xs mt-3">{cast.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+       <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+          {dummyShowsData.slice(0, 4).map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
+      </div>
+
+      <div className="flex justify-center mt-20">
+        <button onClick={() => {navigate('/movies'); scrollTo(0,0)}} className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">Show more</button>
+      </div>
+
     </div>
   ) : <Loading />
 }
